65086c68bdd61d63f838cd8ef0a245ff630a095f
Vera Phipps <85977298+veraphipps01@users.noreply.github.com>
2022-05-23 13:55:10 -0700

    Add files via upload

---
+++ frontend/pages/checkout.js
@@ -0,0 +1,37 @@
+/* pages/checkout.js */
+
+import React, { useContext } from "react";
+import { Row, Col } from "reactstrap";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "../components/checkoutForm";
+import AppContext from "../components/context";
+import Cart from "../components/cart";
+
+function Checkout() {
+  // get app context
+  const {isAuthenticated} = useContext(AppContext);
+  // isAuthenticated is passed to the cart component to display order button
+  //const isAuthenticated  = true;
+  
+  // load stripe to inject into elements components
+  const stripePromise = loadStripe(
+    "pk_test_51L0ya6Erd04QM8YBZZZhejFZ7NekU6E6euMaJxOjgllzhZSAeOqJEI9Cl6RKjW52qMw8FcqVpa2VRjxWTiKzmDBy00hpBfRjnt"
+  );
+
+  return (
+    <Row>
+      <Col style={{ paddingRight: 0 }} sm={{ size: 3, order: 1, offset: 2 }}>
+        <h1 style={{ margin: 20 }}>Checkout</h1>
+        <Cart isAuthenticated={isAuthenticated} />
+      </Col>
+      <Col style={{ paddingLeft: 5 }} sm={{ size: 6, order: 2 }}>
+        <Elements stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      </Col>
+    </Row>
+  );
+  // }
+}
+export default Checkout;
